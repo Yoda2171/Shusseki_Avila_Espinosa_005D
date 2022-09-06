98bac6cc10caee2ae47b4427a3b49541f727f688
unknown <pablo.avila2171@gmail.com>
2022-09-06 06:36:40 -0300

    Estudiantes avance

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: 'login',
@@ -31,6 +32,11 @@ const routes: Routes = [
     path: 'code-qr',
     loadChildren: () => import('./pages/code-qr/code-qr.module').then( m => m.CodeQRPageModule)
   },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
+interface Componente {
+  icon: string;
+  name: string;
+  redirecTo: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  componentes: Componente[] = [
+    {
+      icon: 'pencil',
+      name: 'Estudiantes',
+      redirecTo: '/estudiantes',
+    },
+    {
+      icon: 'person',
+      name: 'Profesores',
+      redirecTo: '/profesores',
+    },
+    {
+      icon: 'qr-code',
+      name: 'Code-QR',
+      redirecTo: '/code-qr',
+    },
+    {
+      icon: 'book',
+      name: 'Cursos',
+      redirecTo: '/cursos',
+    },
+  ];
   constructor() {}
+
 }
